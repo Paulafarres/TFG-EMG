35c20f7e56088c260bc9d93f0d541e685c12b9a6
Paulafarres <162596659+Paulafarres@users.noreply.github.com>
2025-05-28 17:37:36 +0200

    Create main.cpp

---
+++ test/validacio_rms/main.cpp
@@ -0,0 +1,227 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+#include <math.h>
+
+using namespace std;
+volatile bool novesdades = false;
+
+#define SERVICE_UUID "0af56af5-502d-4244-ae1d-262a9d72945d"
+#define CHARACTERISTIC_UUID "4fc085b6-f4a6-4142-ac13-ced0cf3ad0e3"
+
+BLECharacteristic* pCharacteristic;
+bool deviceConnected = false;
+
+// Definició de pins i constants
+#define MOSI 23
+#define MISO 19
+#define SCLK 18
+#define SS 5
+
+#define CLKSEL 33
+#define DRDY 26
+#define START 27
+#define RESET 14
+#define PWDN 12
+
+// Comandes ADS1294
+#define CMD_READ_REG 0x20
+#define CMD_WRITE_REG 0x40
+#define CMD_STOP 0x0A
+#define CMD_SDATAC 0x11
+#define CMD_RDATAC 0x10
+#define CMD_RESET 0x06
+
+// Configuració de registres ADS1294
+#define CONFIG1_REG 0x01
+#define CONFIG1_VALUE 0xC6
+#define CONFIG2_REG 0x02
+#define CONFIG2_VALUE 0x40
+#define CONFIG3_REG 0x03
+#define CONFIG3_VALUE 0xE0
+#define LOFF_REG 0x04
+#define LOFF_VALUE 0x00
+#define CH1SET_REG 0x05
+#define CH1SET_VALUE 0x60
+#define CH2SET_REG 0x06
+#define CH2SET_VALUE 0x60
+#define CH3SET_REG 0x07
+#define CH3SET_VALUE 0x60
+#define CH4SET_REG 0x08
+#define CH4SET_VALUE 0x00
+#define RLD_SENSP_REG 0x0D
+#define RLD_SENSP_VALUE 0x07
+#define RLD_SENSN_REG 0x0E
+#define RLD_SENSN_VALUE 0x07
+#define GPIO_REG_ADS 0x14
+#define GPIO_VALUE_ADS 0x0F
+#define CONFIG4_REG 0x17
+#define CONFIG4_VALUE 0x00
+#define WCT1_REG 0x18
+#define WCT1_VALUE 0x00
+#define WCT2_REG 0x19
+#define WCT2_VALUE 0x00
+
+volatile bool novaMostra = false;
+
+// --- Funcions d’escriptura/lectura de registres ADS1294 ---
+void ads1294_writeCommand(uint8_t cmd) {
+  digitalWrite(SS, LOW);
+  delayMicroseconds(2);
+  SPI.transfer(cmd);
+  delayMicroseconds(2);
+  digitalWrite(SS, HIGH);
+  delay(1);
+}
+
+uint8_t ads1294_readRegister(uint8_t reg_addr) {
+  digitalWrite(SS, LOW);
+  delayMicroseconds(2);
+  SPI.transfer(CMD_READ_REG | reg_addr);
+  SPI.transfer(0x00);
+  uint8_t value = SPI.transfer(0x00);
+  delayMicroseconds(2);
+  digitalWrite(SS, HIGH);
+  delay(1);
+  return value;
+}
+
+void ads1294_writeRegister(uint8_t reg_addr, uint8_t value) {
+  digitalWrite(SS, LOW);
+  delayMicroseconds(2);
+  SPI.transfer(CMD_WRITE_REG | reg_addr);
+  SPI.transfer(0x00);
+  SPI.transfer(value);
+  delayMicroseconds(2);
+  digitalWrite(SS, HIGH);
+  delay(1);
+}
+
+void configRegisters() {
+  digitalWrite(SS, LOW);
+  delayMicroseconds(2);
+  ads1294_writeRegister(CONFIG1_REG, CONFIG1_VALUE);
+  ads1294_writeRegister(CONFIG2_REG, CONFIG2_VALUE);
+  ads1294_writeRegister(CONFIG3_REG, CONFIG3_VALUE);
+  ads1294_writeRegister(LOFF_REG, LOFF_VALUE);
+  ads1294_writeRegister(CH1SET_REG, CH1SET_VALUE);
+  ads1294_writeRegister(CH2SET_REG, CH2SET_VALUE);
+  ads1294_writeRegister(CH3SET_REG, CH3SET_VALUE);
+  ads1294_writeRegister(CH4SET_REG, CH4SET_VALUE);
+  ads1294_writeRegister(RLD_SENSP_REG, RLD_SENSP_VALUE);
+  ads1294_writeRegister(RLD_SENSN_REG, RLD_SENSN_VALUE);
+  ads1294_writeRegister(GPIO_REG_ADS, GPIO_VALUE_ADS);
+  ads1294_writeRegister(CONFIG4_REG, CONFIG4_VALUE);
+  ads1294_writeRegister(WCT1_REG, WCT1_VALUE);
+  ads1294_writeRegister(WCT2_REG, WCT2_VALUE);
+  delayMicroseconds(2);
+  digitalWrite(SS, HIGH);
+}
+
+// --- Conversió a mil·livolts ---
+float convertToMillivolts(int32_t valor, float vref = 2.4, int gain = 1) {
+  const float FS = 8388607.0;
+  return (valor * vref * 1000.0) / (FS * gain);
+}
+
+// --- Eliminació d’offset ---
+#define OFFSET_WINDOW 50
+float offset_buffer[OFFSET_WINDOW];
+int offset_index = 0;
+float offset_sum = 0;
+
+float removeOffset(float x) {
+  offset_sum -= offset_buffer[offset_index];
+  offset_buffer[offset_index] = x;
+  offset_sum += x;
+  offset_index = (offset_index + 1) % OFFSET_WINDOW;
+  return x - (offset_sum / OFFSET_WINDOW);
+}
+
+// --- Mitjana mòbil ---
+#define MOVING_AVG_WINDOW 5
+float moving_avg_buffer[MOVING_AVG_WINDOW];
+int moving_avg_index = 0;
+
+float moving_average(float nova) {
+  moving_avg_buffer[moving_avg_index] = nova;
+  moving_avg_index = (moving_avg_index + 1) % MOVING_AVG_WINDOW;
+  float suma = 0;
+  for (int j = 0; j < MOVING_AVG_WINDOW; j++) suma += moving_avg_buffer[j];
+  return suma / MOVING_AVG_WINDOW;
+}
+
+
+// --- Càlcul RMS amb finestra ---
+#define RMS_WINDOW 30
+float finestra_rms[RMS_WINDOW];
+int idx_rms = 0;
+bool finestra_plena = false;
+
+float calcularRMS(float nova_mostra) {
+  finestra_rms[idx_rms] = nova_mostra;
+  idx_rms = (idx_rms + 1) % RMS_WINDOW;
+  if (idx_rms == 0) finestra_plena = true;
+
+  float suma = 0;
+  int finestra_actual = finestra_plena ? RMS_WINDOW : idx_rms;
+
+  for (int k = 0; k < finestra_actual; k++) {
+    suma += finestra_rms[k] * finestra_rms[k];
+  }
+
+  return sqrt(suma / finestra_actual);
+}
+
+// --- ISR per DRDY ---
+void IRAM_ATTR drdyISR() {
+  novaMostra = true;
+}
+
+// --- Setup ---
+void setup() {
+  Serial.begin(115200);
+  pinMode(MISO, INPUT); pinMode(MOSI, OUTPUT);
+  pinMode(SCLK, OUTPUT); pinMode(SS, OUTPUT);
+  pinMode(DRDY, INPUT); pinMode(RESET, OUTPUT);
+  pinMode(PWDN, OUTPUT); pinMode(START, OUTPUT);
+  pinMode(CLKSEL, OUTPUT); digitalWrite(CLKSEL, HIGH);
+
+  digitalWrite(PWDN, LOW); delay(1);
+  digitalWrite(PWDN, HIGH); delay(10);
+  digitalWrite(RESET, LOW); delay(1);
+  digitalWrite(RESET, HIGH); delay(10);
+
+  SPI.begin(SCLK, MISO, MOSI, SS);
+  SPI.beginTransaction(SPISettings(100000, MSBFIRST, SPI_MODE1));
+  ads1294_writeCommand(CMD_RESET);
+  delay(100);
+  configRegisters();
+  digitalWrite(START, HIGH); delay(1);
+  attachInterrupt(digitalPinToInterrupt(DRDY), drdyISR, FALLING);
+}
+
+// --- Loop principal ---
+void loop() {
+  if (novaMostra) {
+    novaMostra = false;
+    uint8_t canal1[3];
+    digitalWrite(SS, LOW);
+    for (int i = 0; i < 3; i++) SPI.transfer(0x00);  // STATUS
+    for (int i = 0; i < 3; i++) canal1[i] = SPI.transfer(0x00);
+    digitalWrite(SS, HIGH);
+
+    int32_t valor = ((uint32_t)canal1[0] << 16) | ((uint32_t)canal1[1] << 8) | canal1[2];
+    if (valor & 0x800000) valor |= 0xFF000000;
+
+    float mv = convertToMillivolts(valor);
+    float mv_sense_offset = removeOffset(mv);
+    float mv_suau = moving_average(mv_sense_offset);
+    float rms = calcularRMS(mv_suau);
+
+    Serial.println(rms);
+  }
+}
